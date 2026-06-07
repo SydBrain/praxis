16168d93b63713349f9c2e811e3735f0e5cce63f
Pio Musco <piomusco99@gmail.com>
2026-06-07 12:15:21 +0200

    Initial Commit

---
+++ src/test/java/com/praxis/PraxisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praxis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraxisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
